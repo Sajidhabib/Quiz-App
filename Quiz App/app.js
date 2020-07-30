e9e4bc3fd607fd8187aff4718b959e57622ed32a
sajid <brohisajid786@gmail.com>
2020-07-30 02:37:21 -0700

    Javascript app

---
+++ Quiz App/app.js
@@ -0,0 +1,172 @@
+function check (){
+
+  var rightAns_1 = document.getElementById('q1-a2')
+  var rightAns_2 = document.getElementById('q1-a1')
+  var rightAns_3 = document.getElementById('q1-a3')
+  var rightAns_4 = document.getElementById('q1-a4')
+  var score;
+  if (rightAns_1.checked == true)
+  
+  {
+    score++
+      alert("Your Answer of Q1 is Right ")
+  }
+  else {
+      alert("Answer of Question 1 is wrong ")
+
+
+}
+  var rightAns_2 = document.getElementById('q2-a1')
+ 
+  var que_3 = document.getElementById('q2-a2')
+  var que_2 = document.getElementById('q2-a3')
+  var que_4 = document.getElementById('q2-a4')
+  if (rightAns_2.checked == true)
+  
+  {
+    score++
+
+      alert("Your Answer of Q2 is Right ")
+  }
+  else {
+      alert("Answer of Question 2 is wrong ")
+
+
+}
+var rightAns_3 = document.getElementById('q3-a3')
+ 
+  var que_3ss = document.getElementById('q3-a2')
+  var que_2ss = document.getElementById('q3-a1')
+  var que_4ss = document.getElementById('q3-a4')
+  if (rightAns_3.checked == true)
+  
+  {
+    score++
+
+      alert("Your Answer of Q3 is Right ")
+  }
+  else {
+      alert("Answer of Question 3 is wrong ")
+
+
+}
+var rightAns_4 = document.getElementById('q4-a2')
+ 
+  var ques_3 = document.getElementById('q4-a1')
+  var ques_2 = document.getElementById('q4-a3')
+  var ques_4 = document.getElementById('q4-a4')
+  if (rightAns_4.checked == true)
+  
+  {
+    score++
+
+      alert("Your Answer of Q4 is Right ")
+  }
+  else {
+      alert("Answer of Question 4 is wrong ")
+
+
+}
+var rightAns_5 = document.getElementById('q5-a2')
+ 
+var quess_3 = document.getElementById('q5-a1')
+var quess_2 = document.getElementById('q5-a3')
+var quess_4 = document.getElementById('q5-a4')
+if (rightAns_5.checked == true)
+
+{
+  score++
+
+    alert("Your Answer of Q5 is Right ")
+}
+else {
+    alert("Answer of Question 5 is wrong ")
+
+
+}
+
+var rightAns_6 = document.getElementById('q6-a1')
+ 
+var que6_3 = document.getElementById('q6-a2')
+var que6_2 = document.getElementById('q6-a3')
+var que6_4 = document.getElementById('q6-a4')
+if (rightAns_6.checked == true)
+
+{
+  score++
+
+    alert("Your Answer of Q6 is Right ")
+}
+else {
+    alert("Answer of Question 6 is wrong ")
+
+
+}
+var rightAns_7 = document.getElementById('q7-a2')
+ 
+var que7_3 = document.getElementById('q7-a1')
+var que7_2 = document.getElementById('q7-a3')
+var que7_4 = document.getElementById('q7-a4')
+if (rightAns_7.checked)
+
+{
+  score++
+
+    alert("Your Answer of Q7 is Right ")
+}
+else {
+    alert("Answer of Question 7 is wrong ")
+
+
+}
+var rightAns_8 = document.getElementById('q8-a2')
+ 
+var que8_3 = document.getElementById('q8-a1')
+var que8_2 = document.getElementById('q8-a3')
+var que8_4 = document.getElementById('q8-a4')
+if (rightAns_8.checked == true)
+
+{
+  score++
+
+    alert("Your Answer of Q8 is Right ")
+}
+else {
+    alert("Answer of Question 8 is wrong ")
+
+
+}
+var rightAns_9 = document.getElementById('q9-a4')
+ 
+var que9_3 = document.getElementById('q9-a2')
+var que9_2 = document.getElementById('q9-a3')
+var que9_4 = document.getElementById('q9-a4')
+if (rightAns_9.checked == true)
+
+{
+  score++
+    alert("Your Answer of Q9 is Right ")
+}
+else {
+    alert("Answer of Question 9 is wrong ")
+
+
+}
+var rightAns_10 = document.getElementById('q10-a2')
+ 
+var que8_3 = document.getElementById('q10-a1')
+var que8_2 = document.getElementById('q10-a3')
+var que8_4 = document.getElementById('q10-a4')
+if (rightAns_10.checked == true)
+
+{
+  score++
+ alert("Your Answer of Q10 is Right ")
+}
+else {
+    alert("Answer of Question 10 is wrong ")
+
+
+}
+                 alert("your Score is " + score++)
+}
